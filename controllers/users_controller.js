6cfd0bf635ba4231732af7063ae39bc137fc7c77
chahat <chahatba9876@gmail.com>
2023-06-13 21:05:01 +0530

    sign in and sign up forms created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,3 +2,28 @@ module.exports.profile=function(req,res)
 {
     res.end('<h1>User profile</h1>');
 };
+
+module.exports.signUp=function(req,res)
+{
+    res.render('user_sign_up',{
+        title:"Codial | Sign Up"
+    });
+};
+
+//render sign in page
+module.exports.signIn=function(req,res)
+{
+    res.render('user_sign_in',{
+        title:"Codial | Sign In"
+    });
+};
+
+module.exports.create=function(req,res)
+{
+
+}
+
+module.exports.create_session=function(req,res)
+{
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const homeController=require('../controllers/home_controller');
 
 
 router.get('/',homeController.home);
-router.use('/user',require('./users'));
+router.use('/users',require('./users'));
 
 module.exports=router;
